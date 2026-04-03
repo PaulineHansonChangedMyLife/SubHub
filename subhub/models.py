382a80bdee29060befc829cac752352fabdc3a1e
PaulineHansonChangedMyLife <owlcollins11@gmail.com>
2026-04-04 02:06:08 +1100

    Added subscription model

--- subhub/models.py
+++ subhub/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Subscription(models.Model): # Subscription model | Needed to track every users subscriptions
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+
+    class reocurrance(models.TextChoices):
+        daily = 'daily', 'daily'
+        weekly = 'weekly', 'weekly'
+        biweekly = 'biweekly', 'biweekly'
+        monthly = 'monthly', 'monthly'
+        trimonthly = 'trimonthly', 'trimonthly'
+        biyearly = 'biyearly', 'biyearly'
+        anually = 'anually', 'anually'
+
+    class category(models.TextChoices):
+        fixed = 'fixed', 'fixed'
+        entertainment = 'entertainment', 'entertainment'
+        groceries = 'groceries', 'groceries'
+        AI = 'AI', 'AI'
+        education = 'education', 'education'
+        loans = 'loans', 'loans'
+        other = 'other', 'other'
+
+    created_on = models.DateTimeField(auto_now_add=True)
--- subhub/views.py
+++ subhub/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+#from .models import testModel
+#from .forms import testForm
 
 @login_required(login_url='users:login')
 def home(request):
